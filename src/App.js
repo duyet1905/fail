c2c50c7208731cc46b9c0cff2e7a657edf8dc919
Thế Duyệt Phạm <duyet1905@gmail.com>
2021-04-01 17:30:18 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { render } from "react-dom";
+import { Table } from "rsuite-table";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const styles = {
+  padding: 20,
+  textAlign: "center"
+};
 
+const App = () => (
+  <div style={styles}>
+    <Table>Hello World</Table>
+  </div>
+);
+
+render(<App />, document.getElementById("root"));
 export default App;
